fd1a98ea37e015e3ad7c0a79f06bf0f55d6d679e
Sujeet Gund <sujeetgund@gmail.com>
2025-04-04 19:36:21 +0530

    Create Data Ingestion Config

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+import pandas as pd
+
+
+# Common constants for the training pipeline
+PIPELINE_NAME: str = "network_security"
+TARGET_COLUMN: str = "Result"
+ARTIFACTS_NAME: str = "artifacts"
+
+FEATURE_STORE_FILE_NAME: str = "network_data.csv"
+TRAIN_DATA_FILE_NAME: str = "train.csv"
+TEST_DATA_FILE_NAME: str = "test.csv"
+
+
+# Data Ingestion related constants start with "DATA_INGESTION" variable name
+DATA_INGESTION_DATABASE_NAME: str = "network_security"
+DATA_INGESTION_COLLECTION_NAME: str = "network_data"
+DATA_INGESTION_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_NAME: str = "feature_store"
+DATA_INGESTION_INGESTED_NAME: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_RATIO: float = 0.2
---
+++ networksecurity/entity/config_entity.py
@@ -0,0 +1,37 @@
+import os
+from datetime import datetime
+
+from networksecurity.constant import training_pipeline
+
+
+class TrainingPipelineConfig:
+    def __init__(self):
+        timestamps = datetime.now().strftime("%Y%m%d%H%M%S")
+        self.pipeline_name = training_pipeline.PIPELINE_NAME
+        self.artifact_name = training_pipeline.ARTIFACTS_NAME
+        self.artifact_dir = os.path.join(training_pipeline.ARTIFACTS_NAME, timestamps)
+
+
+class DataIngestionConfig:
+    def __init__(self, config: TrainingPipelineConfig):
+        self.data_ingestion_dir = os.path.join(
+            config.artifact_dir, training_pipeline.DATA_INGESTION_NAME
+        )
+        self.feature_store_file_path = os.path.join(
+            self.data_ingestion_dir,
+            training_pipeline.DATA_INGESTION_FEATURE_STORE_NAME,
+            training_pipeline.FEATURE_STORE_FILE_NAME,
+        )
+        self.training_file_path = os.path.join(
+            self.data_ingestion_dir,
+            training_pipeline.DATA_INGESTION_INGESTED_NAME,
+            training_pipeline.TRAIN_DATA_FILE_NAME,
+        )
+        self.testing_file_path = os.path.join(
+            self.data_ingestion_dir,
+            training_pipeline.DATA_INGESTION_INGESTED_NAME,
+            training_pipeline.TEST_DATA_FILE_NAME,
+        )
+        self.database_name = training_pipeline.DATA_INGESTION_DATABASE_NAME
+        self.collection_name = training_pipeline.DATA_INGESTION_COLLECTION_NAME
+        self.train_test_ratio = training_pipeline.DATA_INGESTION_TRAIN_TEST_RATIO
