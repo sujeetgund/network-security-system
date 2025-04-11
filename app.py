f544fdbde3fe778940ddfba9acb49b5af64bbd23
Sujeet Gund <sujeetgund@gmail.com>
2025-04-12 00:02:14 +0530

    Create training pipeline and fastapi app

---
+++ app.py
@@ -0,0 +1,80 @@
+import os
+import pandas as pd
+import numpy as np
+from pymongo.mongo_client import MongoClient
+import certifi
+from dotenv import load_dotenv
+from uvicorn import run as app_run
+
+from fastapi import FastAPI, Request, UploadFile, File
+from fastapi.responses import RedirectResponse, Response, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.pipelines.training_pipeline import TrainingPipeline
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging import logger
+from networksecurity.utils.main_utils import load_object
+
+
+ca = certifi.where()
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI, tlsCAFile=ca)
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def index():
+    return RedirectResponse(url="/docs")
+
+
+@app.get("/train")
+def train_route():
+    try:
+        training_pipeline_config = TrainingPipelineConfig()
+        train_pipeline = TrainingPipeline(config=training_pipeline_config)
+        train_pipeline.run_pipeline()
+        return Response("Training is successful")
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+        raise NetworkSecurityException(e)
+
+
+@app.post("/predict")
+def predict_route(request: Request, file: UploadFile = File(...)):
+    try:
+        df = pd.read_csv(file.file)
+        if df.empty:
+            return Response("Empty dataset provided for prediction.")
+
+        network_model: NetworkModel = load_object(
+            filepath=os.path.join("final_model", "model.pkl")
+        )
+
+        preds = network_model.predict(df)
+        if preds is None:
+            return Response("Prediction failed. Please check the input data.")
+
+        return JSONResponse(content={"predictions": preds.tolist()}, status_code=200)
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+        raise NetworkSecurityException(e)
+
+
+if __name__ == "__main__":
+    app_run(app, host="0.0.0.0", port=8000)
---
+++ networksecurity/pipelines/training_pipeline.py
@@ -0,0 +1,161 @@
+from networksecurity.entity.config_entity import (
+    TrainingPipelineConfig,
+    DataIngestionConfig,
+    DataValidationConfig,
+    DataTransformationConfig,
+    ModelTrainerConfig,
+)
+
+from networksecurity.entity.artifact_entity import (
+    DataIngestionArtifact,
+    DataValidationArtifact,
+    DataTransformationArtifact,
+    ModelTrainerArtifact,
+)
+
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
+
+from networksecurity.exception import NetworkSecurityException
+from networksecurity.logging import logger
+
+
+class TrainingPipeline:
+    def __init__(self, config: TrainingPipelineConfig):
+        self.config = config
+
+    def start_data_ingestion(self) -> DataIngestionArtifact:
+        """
+        This function is responsible for starting the data ingestion process.
+
+        Raises:
+            NetworkSecurityException: If there is an error during the data ingestion process.
+
+        Returns:
+            DataIngestionArtifact: An artifact containing information about the data ingestion process.
+        """
+        try:
+            data_ingestion_config = DataIngestionConfig(config=self.config)
+            data_ingestion = DataIngestion(config=data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.init_data_ingestion()
+
+            return data_ingestion_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e)
+
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        """
+        This function is responsible for starting the data validation process.
+        It takes the data ingestion artifact as input and performs validation checks on the ingested data.
+
+        Args:
+            data_ingestion_artifact (DataIngestionArtifact): An artifact containing information about the data ingestion process.
+
+        Raises:
+            NetworkSecurityException: If there is an error during the data validation process.
+
+        Returns:
+            DataValidationArtifact: An artifact containing information about the data validation process.
+        """
+        try:
+            data_validation_config = DataValidationConfig(config=self.config)
+            data_validation = DataValidation(
+                config=data_validation_config,
+                data_ingestion_artifact=data_ingestion_artifact,
+            )
+            data_validation_artifact = data_validation.init_data_validation()
+
+            return data_validation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e)
+
+    def start_data_transformation(
+        self, data_validation_artifact: DataValidationArtifact
+    ) -> DataTransformationArtifact:
+        """
+        This function is responsible for starting the data transformation process.
+        It takes the data validation artifact as input and performs transformation operations on the validated data.
+
+        Args:
+            data_validation_artifact (DataValidationArtifact): An artifact containing information about the data validation process.
+
+        Raises:
+            NetworkSecurityException: If there is an error during the data transformation process.
+
+        Returns:
+            DataTransformationArtifact: An artifact containing information about the data transformation process.
+        """
+        try:
+            data_transformation_config = DataTransformationConfig(config=self.config)
+            data_transformation = DataTransformation(
+                config=data_transformation_config,
+                data_validation_artifact=data_validation_artifact,
+            )
+            data_transformation_artifact = (
+                data_transformation.init_data_transformation()
+            )
+
+            return data_transformation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e)
+
+    def start_model_trainer(
+        self, data_transformation_artifact: DataTransformationArtifact
+    ) -> ModelTrainerArtifact:
+        """
+        This function is responsible for starting the model training process.
+        It takes the data transformation artifact as input and trains a machine learning model.
+
+        Args:
+            data_transformation_artifact (DataTransformationArtifact): An artifact containing information about the data transformation process.
+
+        Raises:
+            NetworkSecurityException: If there is an error during the model training process.
+
+        Returns:
+            ModelTrainerArtifact: An artifact containing information about the model training process.
+        """
+        try:
+            model_trainer_config = ModelTrainerConfig(config=self.config)
+            model_trainer = ModelTrainer(
+                config=model_trainer_config,
+                data_transformation_artifact=data_transformation_artifact,
+            )
+            model_trainer_artifact = model_trainer.init_model_trainer()
+
+            return model_trainer_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e)
+
+    def run_pipeline(self) -> ModelTrainerArtifact:
+        """
+        This function is responsible for running the entire training pipeline.
+        It orchestrates the execution of various components such as data ingestion,
+        data validation, data transformation, and model training.
+        It returns the final model trainer artifact.
+
+        Raises:
+            NetworkSecurityException: If there is an error during executing the training pipeline.
+
+        Returns:
+            ModelTrainerArtifact: An artifact containing information about the model training process.
+        """
+        try:
+            data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
+            data_transformation_artifact = self.start_data_transformation(
+                data_validation_artifact=data_validation_artifact
+            )
+            model_trainer_artifact = self.start_model_trainer(
+                data_transformation_artifact=data_transformation_artifact
+            )
+
+            return model_trainer_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e)
